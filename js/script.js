63f466ee60d4a5920df14abb7e7165a43b397699
itsbengray <102569997+itsbengray@users.noreply.github.com>
2022-03-29 12:45:54 +0800

    Added task

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// Empty script file to start with
+// Step 1: Find Element
+let addTaskbutton = document.getElementById("add-task");
+
+let newTaskInput = document.getElementById("task-input");
+
+// Step 2: Write a function to implement the behaviour
+function onAddTaskClicked(event){
+    let taskName = newTaskInput.value;
+    newTaskInput.value = "";
+    console.log(taskName);
+}
+
+// Step 3: Link element, function and event
+addTaskbutton.addEventListener('click',onAddTaskClicked);
