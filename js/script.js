a3d2c47cde7b4fd47f45731891f5aac776e7e42b
itsbengray <102569997+itsbengray@users.noreply.github.com>
2022-03-29 13:34:48 +0800

    Using HTML template

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
-// Step 1: Find Element
+// Step 1: Find Element by ID
 let addTaskbutton = document.getElementById("add-task");
 
 let newTaskInput = document.getElementById("task-input");
 
 let todoListContainer = document.getElementById("todo-list");
 
+let templateElement = document.getElementById("list-item-template");
+
+let template = templateElement.innerHTML;
+
 // Step 2: Write a function to implement the behaviour
 function onAddTaskClicked(event){
     let taskName = newTaskInput.value;
     newTaskInput.value = "";
-    console.log(taskName);
-    todoListContainer.insertAdjacentHTML("afterbegin",taskName);
+
+
+    let taskHTML = template.replace("<!--Task_Name-->",taskName);
+    todoListContainer.insertAdjacentHTML("afterbegin",taskHTML);
 }
 
-// Step 3: Link element, function and event
+// Step 3: Link element, function and event, event listener
 addTaskbutton.addEventListener('click',onAddTaskClicked);
